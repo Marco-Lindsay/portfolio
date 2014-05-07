1c7c887a8af6c90eb76644a8b9e4632bd21454bd
marco-lindsay <lindsay_marco@hotmail.com>
2014-05-07 16:38:18 -0700

    added destroy test

---
+++ test/features/deleting_a_post_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+feature "DeletingAPost" do
+  scenario "Druken rant can be expunged" do
+    post = Post.create(title: "this is the title", body: "this is the body")
+    visit root_path
+    click_on "Destroy"
+
+    page.wont_have_content "this is the title"
+  end
+end
