7b3198469f58478156fac32e955e4195b8d68247
marco-lindsay <lindsay_marco@hotmail.com>
2014-05-07 16:21:42 -0700

    added new post test

---
+++ test/features/creating_a_post_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+feature "CreatingAPost" do
+  scenario "submit form data to create a new post" do
+    visit new_post_path
+    fill_in "Title", with: "Code Rails"
+    fill_in "Body", with: "This is how I learne to make Rails apps."
+    click_on "Create Post"
+    page.text.must_include "Post was succussfully created"
+    page.text.must_include "how I learned to make Rails apps"
+  end
+end
