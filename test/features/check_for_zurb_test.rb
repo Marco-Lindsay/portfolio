774595890ee164d21b4449b2e54aa68def36d7cf
marco-lindsay <lindsay_marco@hotmail.com>
2014-05-08 16:24:57 -0700

    added test to check for zurb

--- test/features/check_for_zurb_test.rb
+++ test/features/check_for_zurb_test.rb
@@ -1,9 +1,23 @@
-require "test_helper"
+# require "test_helper"
+
+# feature "CheckForZurb" do
+#   # given we want a pretty site that is more than basic html or rails
+#   scenario "Check that Zurb is being used for styling" do
+#   # when we visit the root path
+#     visit root_path
+#   # then we want to check that zurb foundation is generating the styling
+#     page.body.must_include"columns"
+#   end
+# end
+
+# require "test_helper"
 
 feature "CheckForZurb" do
-  scenario "the test is sound" do
+  # given we want a pretty site that is more than basic html or rails
+  scenario "Check that Zurb is being used for styling" do
+  # when we visit the root path
     visit root_path
-    page.must_have_content "Hello World"
-    page.wont_have_content "Goobye All!"
+  # then we want to check that zurb foundation is generating the styling
+    page.html.must_include"foundation_and_overrides.css"
   end
 end
