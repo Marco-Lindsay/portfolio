4c1943f03e047843eb5587d1d0db0131a8892dff
marco-lindsay <lindsay_marco@hotmail.com>
2014-05-12 15:13:10 -0700

    created a test for projects and the projects controller

---
+++ test/features/projects/creating_a_project_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+feature "Projects::CreatingAProject" do
+  scenario "there is the ability to create a project" do
+    visit projects_path
+    click_on "New Project"
+    fill_in 'Name', with: 'New Project'
+    fill_in 'Technologies used', with: 'Rails, Ruby'
+    click_on "Create Project"
+    page.text_include "Project has been created"
+    assert page.has_css?("#notice"), "Expected a flash notice on this page, none found."
+    page.status_code.must_equal 200
+  end
+end
