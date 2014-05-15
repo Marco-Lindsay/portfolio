7559334a0816fb8736eb8e11ce9b068953bbebf7
marco-lindsay <lindsay_marco@hotmail.com>
2014-05-14 19:42:29 -0700

    cleaning up whitespace

--- test/features/auth/sign_out_user_test.rb
+++ test/features/auth/sign_out_user_test.rb
@@ -7,15 +7,9 @@ feature "Once you sign in as a user you are able to sign out" do
   fill_in "Email", with: users(:marco).email
   fill_in "Password", with: '12345678'
   click_button "Sign in"
-
   page.must_have_content "Signed in successfully."
 
   click_link "Sign out"
   page.must_have_content "Signed out successfully."
-
-
-
-
-
   end
 end
