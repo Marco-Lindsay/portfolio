eac96fa69322aa2fc76716c0373a2fc26d1a6261
marco-lindsay <lindsay_marco@hotmail.com>
2014-06-09 15:36:20 -0700

    sending with postmark in production

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -10,4 +10,13 @@ feature UserMailer do
     page.must_have_content "Your message has been sent!"
 
   end
+
+  scenario "contact will require all fields" do
+    visit '/contact'
+    fill_in "name", with: "marco"
+    fill_in "message", with: "really long message"
+    click_on "Send"
+    page.must_have_content "Please note that all fields are required"
+
+  end
 end
